3ee30f12232d08550b3b8b79cf2cd5aa903ca270
Aegistahl <iant@jenixstudios.com>
2023-09-22 15:46:25 -0500

    first commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace RankingAppTest.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{
+
+		}
+
+
+		public DbSet<ItemModel> ItemModels => Set<ItemModel>();
+
+	}
+}
+
